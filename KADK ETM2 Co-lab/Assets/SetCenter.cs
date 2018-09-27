418f10d318e64cf033e05216ca4bf2e5a4a4a036
nikolajmunk <munk.nikolaj@gmail.com>
2018-09-27 12:05:56 +0200

    Added animation and camera follow to heroes.

---
+++ KADK ETM2 Co-lab/Assets/SetCenter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCenter : MonoBehaviour {
+
+    public GameObject target;
+    public float zPosition = -10;
+    public Vector2 partyPosition;
+    public Vector2 previousPos;
+
+	Vector2 CalculateCentroid () {
+        int numberOfHeroes = target.transform.childCount;
+        if (numberOfHeroes > 0)
+        {
+            Vector2 centroid = Vector2.zero;
+            foreach (Transform hero in target.transform)
+            {
+                Vector2 heroPos = hero.transform.position;
+                centroid += heroPos;
+            }
+            centroid /= numberOfHeroes;
+            return centroid;
+        }
+        else { return previousPos; }
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        previousPos = transform.position;
+		partyPosition = CalculateCentroid();
+        transform.position = new Vector3(partyPosition.x, partyPosition.y, zPosition);
+	}
+}
