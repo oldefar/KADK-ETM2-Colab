a2727ae6703ade070afe4ceca04544ab1324c130
nikolajmunk <munk.nikolaj@gmail.com>
2018-09-28 12:11:20 +0200

    Added Playmaker action to orphan heroes when they die.

---
+++ KADK ETM2 Co-lab/Assets/OrphanOnDeath.cs
@@ -0,0 +1,28 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using System.Collections;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.GameObject)]
+    [Tooltip("Decouples the GameObject from its parent.")]
+    public class OrphanOnDeath : FsmStateAction
+    {
+        [RequiredField]
+        [Tooltip("The GameObject to orphan.")]
+
+        public FsmOwnerDefault gameObject;
+
+        public override void OnEnter()
+        {
+            GameObject go = (gameObject.OwnerOption == OwnerDefaultOption.UseOwner ? Owner : gameObject.GameObject.Value);
+            if (go.transform.parent != null)
+            {
+                go.transform.parent = null;
+            }
+
+            Finish();
+        }
+    }
+}
