6aaf31b5c0262bedf3898372ca9c4df5c5f949a2
nikolajmunk <munk.nikolaj@gmail.com>
2018-10-01 15:14:27 +0200

    abc

---
+++ KADK ETM2 Co-lab/Assets/Scripts/ResizeController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeController : MonoBehaviour {
+
+    public GameObject heroContainer;
+    public static float currentResolution = Screen.width / Screen.height;
+    float maxX, maxY, minX, minY;
+    public Camera mainCamera;
+    public float defaultCameraHeight; // Default height for the camera.
+    public float defaultCameraWidth() {
+        return defaultCameraHeight * currentResolution;
+    }
+    public float cameraMargin;
+	
+    Vector2 GetHeroBox()
+    {
+        maxX = -999999;
+        maxY = -999999;
+        minX = 999999;
+        minY = 999999;
+        foreach (Transform child in heroContainer.transform)
+        {
+            float childX = child.position.x;
+            float childY = child.position.y;
+            if (childY > maxY) { maxY = childY; }
+            if (childY < minY) { minY = childY; }
+            if (childX > maxX) { maxX = childX; }
+            if (childX < minX) { minX = childX; }
+        }
+        return new Vector2(Mathf.Abs(maxX - minX), Mathf.Abs(maxY - minY));
+    }
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float cameraHeight;
+        Vector2 boundingBox = GetHeroBox();
+        if (heroContainer.transform.childCount == 0)
+        {
+            cameraHeight = defaultCameraHeight;
+        }
+        else
+        {
+            if (boundingBox.x + cameraMargin > defaultCameraWidth())
+            {
+                cameraHeight = (boundingBox.x + cameraMargin) / currentResolution;
+            }
+            else if (boundingBox.y + cameraMargin > defaultCameraHeight)
+            {
+                cameraHeight = boundingBox.y + cameraMargin;
+            }
+            else
+            {
+                cameraHeight = defaultCameraHeight;
+            }
+        }
+        mainCamera.orthographicSize = cameraHeight;
+	}
+}
---
+++ KADK ETM2 Co-lab/Assets/Scripts/SetCenter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCenter : MonoBehaviour {
+
+    public GameObject target;
+    public float zPosition = -10;
+    public Vector2 partyCentroid;
+    public Vector2 previousPos;
+
+	Vector2 CalculateCentroid () {
+        int numberOfHeroes = target.transform.childCount;
+        if (numberOfHeroes > 0)
+        {
+            Vector2 centroid = Vector2.zero;
+            foreach (Transform hero in target.transform)
+            {
+                Vector2 heroPos = hero.transform.position;
+                centroid += heroPos;
+            }
+            centroid /= numberOfHeroes;
+            return centroid;
+        }
+        else { return previousPos; }
+	}
+
+    void Update()
+    {
+        previousPos = transform.position;
+    }
+
+    void LateUpdate () {
+        partyCentroid = CalculateCentroid();
+        transform.position = new Vector3(partyCentroid.x, partyCentroid.y, zPosition);
+	}
+}
