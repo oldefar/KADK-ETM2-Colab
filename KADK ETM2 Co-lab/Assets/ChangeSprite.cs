11a9a85c182b21f1a394ead5573b212e41c79d49
nikolajmunk <munk.nikolaj@gmail.com>
2018-10-01 15:20:40 +0200

    Added door.

---
+++ KADK ETM2 Co-lab/Assets/ChangeSprite.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour {
+
+    public Sprite newSprite;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.GetComponent<PlayerTouch>().touched == true)
+        {
+            transform.GetComponent<SpriteRenderer>().sprite = newSprite;
+        }
+	}
+}
---
+++ KADK ETM2 Co-lab/Assets/PlayerTouch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTouch : MonoBehaviour {
+
+    public bool touched;
+
+	// Use this for initialization
+	void Start () {
+        touched = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
